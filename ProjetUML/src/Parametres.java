0000915cb181d0ba5f4901b3fba181c17054857f
YangC <yangcao222@gmail.com>
2018-10-23 11:18:39 +0200

    créer new Class Parametres

---
+++ ProjetUML/src/Parametres.java
@@ -0,0 +1,19 @@
+
+public class Parametres {
+	float coefCorrectifV;
+	float coefCorrectifC;
+	
+	public Parametres(float cV, float cC) {
+		this.coefCorrectifC = cC;
+		this.coefCorrectifV = cV;
+	}
+
+	public float getCoefCorrectifV() {
+		return coefCorrectifV;
+	}
+
+	public float getCoefCorrectifC() {
+		return coefCorrectifC;
+	}
+	
+}
